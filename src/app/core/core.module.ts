a3b92a66e13bef576ac8a4fc981a9ba60a17a1f7
Alan Pastor <apastorpina@gmail.com>
2022-09-29 01:01:26 -0500

    refactor: folder structure

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarModule } from './toolbar/toolbar.module';
-import { TabsModule } from './tabs/tabs.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, ToolbarModule, TabsModule],
-  exports: [ToolbarModule, TabsModule],
+  imports: [CommonModule, ToolbarModule,],
+  exports: [ToolbarModule,],
 })
 export class CoreModule {}
